fee22ce1f16996dc068080d768a71c069c35a195
Amanda <a@zalee.me>
2018-09-06 22:47:10 -0700

    Add array partitioning logic

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
@@ -17,11 +18,32 @@ class QuickSort
     self.sort1(left) + [pivot_el] + self.sort1(right)
   end
 
-  # In-place.
+  # In-place
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    return array if array.length < 2
+    pivot_idx = rand(array.length)
+    pivot_el = array[pivot_idx]
+
+    # no new arrays
 
   end
 
   def self.partition(array, start, length, &prc)
+    return if length < 2
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    end_idx = start + length
+    pivot_idx = start
+    barrier_idx = start + 1
+
+    (start + 1...start + length).each do |idx|
+      if prc.call(array[pivot_idx], array[idx]) != -1
+        array[idx], array[barrier_idx] = array[barrier_idx], array[idx]
+        barrier_idx += 1
+      end
+    end
+
+    array[pivot_idx], array[barrier_idx - 1] = array[barrier_idx - 1], array[pivot_idx]
+    pivot_idx = barrier_idx - 1
   end
 end
